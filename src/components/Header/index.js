7712ae7a012f90bd353e3f5032dead71aba79d0f
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-12 13:22:32 -0500

    updating header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header(props) {
 
   return (
     <header className="flex-row space-between px-1">
-      <h1>Lernantino</h1>
+      <h1>Jalon Tuttle</h1>
       {props.children}
     </header>
   );
